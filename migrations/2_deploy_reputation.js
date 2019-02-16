15b90938dfd9c3262aebf13db1d79ed919f88849
hideno <hidenoryo@lifull.com>
2019-02-17 00:25:19 +0900

    デプロイしたコントラクトのアドレスを保存

--- migrations/2_deploy_reputation.js
+++ migrations/2_deploy_reputation.js
@@ -1,7 +1,14 @@
+const fs = require('fs')
+
 const Reputation = artifacts.require("./Reputation.sol");
 
 module.exports = function(deployer) {
-    const v = '100000000';
-    const initialSupply = web3.toHex(v+'0'.repeat(18));
-    deployer.deploy(Reputation, initialSupply);
+  const v = '100000000';
+  const initialSupply = web3.toHex(v+'0'.repeat(18));
+  deployer.then(async () => {
+    const rpt = await deployer.deploy(Reputation, initialSupply);
+    let addresses = require('../work/deployed.json');
+    addresses.Reputation = rpt.address;
+    fs.writeFileSync('./work/deployed.json', JSON.stringify(addresses,null,2))
+  })
 }
--- migrations/3_deploy_user_registry.js
+++ migrations/3_deploy_user_registry.js
@@ -1,14 +1,19 @@
+const fs = require('fs')
+
 const UserRegistry = artifacts.require("./UserRegistry.sol");
 const Reputation = artifacts.require("./Reputation.sol");
 
-
 module.exports = function(deployer) {
   deployer.then(async ()=>{
-
     const rpt = await Reputation.deployed();
     const userReg = await deployer.deploy(UserRegistry, rpt.address);
     const totalSupply = await rpt.totalSupply();
     await rpt.approve(userReg.address, totalSupply);
+
+    let addresses = require('../work/deployed.json');
+    addresses.UserRegistry = userReg.address;
+    fs.writeFileSync('./work/deployed.json', JSON.stringify(addresses,null,2))
+
     return userReg;
   });
 };
