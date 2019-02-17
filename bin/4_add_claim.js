8b53253af705df069e158b409159430c0afbb63c
hideno <hidenoryo@lifull.com>
2019-02-17 12:18:30 +0900

    複数証明書対応

--- bin/4_add_claim.js
+++ bin/4_add_claim.js
@@ -6,99 +6,104 @@ const ARTIFACTS_DIR = '../build/contracts'
 async function main() {
   const web3 = new Web3('http://localhost:7545')
 
-  const accounts = await web3.eth.getAccounts()
-  const issuerClaimsSigner = accounts[1]
-  const subject = accounts[2]
-  console.log('Issuer Claims Signer Address:', issuerClaimsSigner)
-  console.log('Subject Address:', subject)
-
-  const identityArtifact = require(ARTIFACTS_DIR + '/Identity.json')
-
-  const issuerIdentity = new web3.eth.Contract(
-    identityArtifact.abi,
-    deployed.IssuerIdentity
-  )
-  const issuerIdentityAddress = issuerIdentity.options.address
-  console.log('Issuer Identity Contract Address:', issuerIdentityAddress)
-
-  const subjectIdentity = new web3.eth.Contract(
-    identityArtifact.abi,
-    deployed.SubjectIdentity
-  )
-  const subjectIdentityAddress = subjectIdentity.options.address
-  console.log('Subject Identity Contract Address:', subjectIdentityAddress, "\n")
-
-  const claim = 'Subject is very nemui'
-  const hexedData = web3.utils.asciiToHex(claim)
-
-  const hashedDataToSign = web3.utils.soliditySha3(
-    subjectIdentityAddress,
-    7,//CLAIM_TYPES.KYC,
-    hexedData
-  )
-
-  console.log("Sign Hashed Claim ...\n")
-
-/*
-  let signature
-  signature = await web3.eth.sign(
-    hashedDataToSign,
-    issuerClaimsSigner
-  ).catch((error) => {
-    signature = hashedDataToSign
-  })
-*/
-  signature = hashedDataToSign
-  console.log('Signature:', signature, "\n")
-
-  const addClaimABI = subjectIdentity.methods.addClaim(
-    7,//CLAIM_TYPES.KYC,
-    1,//KEY_TYPES.ECDSA,
-    issuerIdentityAddress,
-    signature,
-    hexedData,
-    "https://www.issuer.com/",
-  ).encodeABI()
-  //console.log('addClaimABI:', addClaimABI)
-
-  console.log("Add Claim ...\n")
-
-  await subjectIdentity.methods.execute(
-    subjectIdentityAddress,
-    0,
-    addClaimABI,
-  ).send({
-    gas: 4612388,
-    from: subject
-  }).on('transactionHash', (transactionHash) => {
-    console.log('Transaction Hash:', transactionHash)
-  }).on('receipt', (receipt) => {
-    console.log('Receipt:', receipt)
-  }).catch((error) => {
-    console.error(error)
-  })
-
   const userRegistryArtifact = require(ARTIFACTS_DIR + '/UserRegistry.json')
   const userRegistry = new web3.eth.Contract(
     userRegistryArtifact.abi,
     deployed.UserRegistry
   )
 
-  console.log("\n")
+  const accounts = await web3.eth.getAccounts()
+  const issuers = accounts.slice(0, 5)
+  for (let i=0; i<issuers.length; i++) {
+    console.log(`Issuer Address ${i}: ${issuers[i]}`)
+  }
+  const subjects = accounts.slice(5,10)
+  for (let i=0; i<subjects.length; i++) {
+    console.log(`Subject Address ${i}: ${subjects[i]}`)
+  }
 
-  await userRegistry.methods.registerIssuer(
-    issuerIdentityAddress,
-    subjectIdentityAddress
-  ).send({
-    gas: 4612388,
-    from: subject
-  }).on('transactionHash', (transactionHash) => {
-    console.log('Transaction Hash:', transactionHash)
-  }).on('receipt', (receipt) => {
-    console.log('Receipt:', receipt)
-  }).catch((error) => {
-    console.error(error)
-  })
+  const identityArtifact = require(ARTIFACTS_DIR + '/Identity.json')
+
+  console.log("\nAdd Claims ...\n")
+
+  for (let i=0; i<issuers.length; i++) {
+    console.log(`[${i}]`)
+    const issuerIdentity = new web3.eth.Contract(
+      identityArtifact.abi,
+      deployed.IssuerIdentity[i]
+    )
+    console.log('Issuer Identity Contract Address:', issuerIdentity.options.address)
+
+    const n = Math.floor(Math.random() * (5 - 1+1)) + 1
+    const targets = subjects.slice(0, n)
+    console.log(`Target Subjects [${n}]:`)
+    for (let j=0; j<n; j++) {
+      console.log(`  [${j}]`)
+      const subjectIdentity = new web3.eth.Contract(
+        identityArtifact.abi,
+        deployed.SubjectIdentity[j]
+      )
+      console.log(`  Subject Address:`, targets[j])
+      console.log('  Subject Identity Contract Address:', subjectIdentity.options.address)
+
+      const claim = 'Subject is very nemui'
+      const hexedData = web3.utils.asciiToHex(claim)
+
+      const hashedDataToSign = web3.utils.soliditySha3(
+        subjectIdentity.options.address,
+        7,//CLAIM_TYPES.KYC,
+        hexedData
+      )
+      signature = hashedDataToSign
+
+      console.log("\n  Signature:", signature, "\n")
+
+      const addClaimABI = subjectIdentity.methods.addClaim(
+        7,//CLAIM_TYPES.KYC,
+        1,//KEY_TYPES.ECDSA,
+        issuerIdentity.options.address,
+        signature,
+        hexedData,
+        "https://www.issuer.com/",
+      ).encodeABI()
+      //console.log('addClaimABI:', addClaimABI)
+
+      console.log("  Add Claims ...\n")
+
+      await subjectIdentity.methods.execute(
+        subjectIdentity.options.address,
+        0,
+        addClaimABI,
+      ).send({
+        gas: 4612388,
+        from: targets[j]
+      }).on('transactionHash', (transactionHash) => {
+        console.log('  Transaction Hash:', transactionHash)
+      }).on('receipt', (receipt) => {
+        console.log('  Receipt:', 'AddClaim')
+      }).catch((error) => {
+        console.error(error)
+      })
+
+      console.log("\n")
+
+		/*
+      await userRegistry.methods.registerIssuer(
+        issuerIdentity.options.address,
+        subjectIdentity.options.address
+      ).send({
+        gas: 4612388,
+        from: targets[j]
+      }).on('transactionHash', (transactionHash) => {
+        console.log('  Transaction Hash:', transactionHash)
+      }).on('receipt', (receipt) => {
+        console.log('  Receipt:', 'NewIssuer')
+      }).catch((error) => {
+        console.error(error)
+      })
+	  */
+    }
+  }
 }
 
 main()
